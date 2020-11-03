3341b05cf6a50d2824b075980882ee9817779e9f
xiang0508 <H24663088@mailst.cjcu.edu.tw>
2020-11-03 13:44:56 +0800

    first commit

---
+++ mini_shop/config.php
@@ -0,0 +1,11 @@
+<?php
+define('_SHOP_NAME', 'Tad點心鋪');
+define('_DB_HOST', 'localhost');
+define('_DB_ID', 'root');
+define('_DB_PW', '12345');
+define('_DB_NAME', 'mini_shop');
+define('_UPLOAD_PATH', 'C:/UniServerZ/www/mini_shop/uploads/');
+define('_UPLOAD_URL', 'http://localhost/mini_shop/uploads/');
+
+$admin_array = ['tad'];
+// error_reporting(0);
