ccf05b43ef26be1fccb0fc8ffe99d0df61b16979
xiang0508 <50436122+xiang0508@users.noreply.github.com>
2020-11-03 15:43:23 +0800

    Update config.php

--- mini_shop/config.php
+++ mini_shop/config.php
@@ -1,5 +1,5 @@
 <?php
-define('_SHOP_NAME', 'Tad點心鋪');
+define('_SHOP_NAME', '小明水果店');
 define('_DB_HOST', 'localhost');
 define('_DB_ID', 'root');
 define('_DB_PW', '12345');
@@ -7,5 +7,5 @@ define('_DB_NAME', 'mini_shop');
 define('_UPLOAD_PATH', 'C:/UniServerZ/www/mini_shop/uploads/');
 define('_UPLOAD_URL', 'http://localhost/mini_shop/uploads/');
 
-$admin_array = ['tad'];
+$admin_array = ['a12345'];
 // error_reporting(0);
